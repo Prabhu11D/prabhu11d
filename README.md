a526bfa9235d18a2d56b6f9621fa8b7e308fb1f3
Prabhu <programmerprabhu@gmail.com>
2022-08-09 15:23:42 +0530

    Personal Site Base Initiated - NextJS

---
+++ README.md
@@ -0,0 +1,3 @@
+### Hi there 👋
+
+Currently I'm swimming in Ocean and Enjoying every moment with data.
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Prabhu11D</title>
+        <meta name="description" content="Portfolio Site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Prabhu11D</h1>
+        <p>Here, I'm writing my Journey</p>
+        <div className='tags'>
+          <p className='tag'>Development</p>
+          <p className='tag'>Life</p>
+          <p className='tag'>AI</p>
+        </div>
+      </main>
+    </div>
+  )
+}
